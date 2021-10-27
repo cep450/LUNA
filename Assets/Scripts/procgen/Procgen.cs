7b308c7d27ddc98b14848b914ebfa86a9f1ece97
jaden <cep450@nyu.edu>
2021-10-27 14:06:54 -0400

    procgen now set up to move its child objects

--- Assets/Scripts/procgen/Procgen.cs
+++ Assets/Scripts/procgen/Procgen.cs
@@ -15,7 +15,7 @@ public class Procgen : MonoBehaviour
     //will move all children by this and cull any who have passed the limit
     //or even, move itself by this and set itself back to 0 every so often (moving children accordingly) 
     float cullLimit = 10f;
-    float zerozeroLimit = 1000f;
+    float zerozeroLimit = -1000f;
 
 
     // Start is called before the first frame update
@@ -29,16 +29,18 @@ public class Procgen : MonoBehaviour
     {
         
         //move self by velocity 
-        transform.Translate(Vector3.back * car.getVelocity());
-
+        transform.Translate(Vector3.back * car.getVelocity() * Time.deltaTime);
 
         //check if past limit, move back to 0,0 if so 
-        if(transform.position.z > zerozeroLimit) {
-            transform.Translate(Vector3.back * -1 * zerozeroLimit);
+        if(transform.position.z < zerozeroLimit) {
+            //ITS GETTING HERE BUT SENDING IT BACKWARDS
+            transform.Translate(Vector3.back * zerozeroLimit);
             //update childrens' locations to return them to their original positions
             for(int i = 0; i < transform.childCount; i++) {
-                transform.GetChild(i).Translate(Vector3.back * zerozeroLimit);
-                tryCullChild(i);
+                if(!transform.GetChild(i).Equals(transform)) {
+                    transform.GetChild(i).Translate(Vector3.back * zerozeroLimit);
+                    tryCullChild(i);
+                }
             }
         } else {
             //cull any children past limit 
@@ -53,7 +55,7 @@ public class Procgen : MonoBehaviour
 //basically the inside of a "for each child..." loop, since we can do this in the check if past limit loop to save it from looping twice 
     void tryCullChild(int i) {
         if(transform.GetChild(i).position.z > cullLimit) {
-
+            //TODO delete child obj 
         }
     }
 }
