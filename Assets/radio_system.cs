a1efbc325d09bb4695dfc334b6c833313a76cf45
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2021-10-31 21:32:11 -0400

    new_asset_update

---
+++ Assets/radio_system.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class radio_system : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static float frequency;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
