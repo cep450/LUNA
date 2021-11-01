0ace0814d5bc7dc93b9f7f56487801f0b3156e71
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2021-10-31 23:24:37 -0400

    radio_1

--- Assets/radio_system.cs
+++ Assets/radio_system.cs
@@ -1,19 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class radio_system : MonoBehaviour
 {
     // Start is called before the first frame update
-    public static float frequency;
+    public static float frequency=100.0f;
+    public TextMeshPro textmeshpro;
+    public AudioClip[] radioclips;
+    public AudioClip static_noise;
+    private AudioSource radio;
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+
+        textmeshpro.text = frequency+"FM";
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            frequency = frequency - 0.1f;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            frequency = frequency + 0.1f;
+        }
+
+      
     }
 }
