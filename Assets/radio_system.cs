246281695286a0fa06e437a549da6b30c2558e6d
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2021-11-01 00:20:20 -0400

    radio_protytpe

--- Assets/radio_system.cs
+++ Assets/radio_system.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Audio;
+
 
 public class radio_system : MonoBehaviour
 {
     // Start is called before the first frame update
     public static float frequency=100.0f;
     public TextMeshPro textmeshpro;
-    public AudioClip[] radioclips;
-    public AudioClip static_noise;
-    private AudioSource radio;
+   // public AudioClip[] radioclips;
+   // public AudioClip static_noise;
+  //  private AudioSource radio;
+    public AudioMixer masterMixer;
     void Start()
     {
        
@@ -25,12 +28,18 @@ public class radio_system : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             frequency = frequency - 0.1f;
+            SetSound(frequency-100.0f);
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             frequency = frequency + 0.1f;
+            SetSound(frequency - 100.0f);
         }
 
-      
+       
+    }
+    public void SetSound(float soundLevel)
+    {
+        masterMixer.SetFloat("SFX_VOL", soundLevel);
     }
 }
