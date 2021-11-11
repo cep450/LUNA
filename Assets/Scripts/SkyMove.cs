5c87bbf23354699f721bc9c9237580f3565d47f8
jaden <cep450@nyu.edu>
2021-11-11 12:44:15 -0500

    proc tuning, fixed radio collider but not location

--- Assets/Scripts/SkyMove.cs
+++ Assets/Scripts/SkyMove.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class SkyMove : MonoBehaviour
 {
 
-    float rate = 0.1f;
+    float rate = 0.15f;
     //float max = -1f;
 
     public bool skyforever;
-    float skymin = -30;
-    float skymoveup = 170;
+    float skymin = -130;
+    float skymoveup = 130 + 364;
 
     // Update is called once per frame
     void Update()
@@ -18,7 +18,7 @@ public class SkyMove : MonoBehaviour
         //if(transform.position.y > max) {
             transform.Translate(Vector3.down * (rate * Time.deltaTime));
         //}
-        if(skyforever && transform.position.y > skymin) {
+        if(skyforever && transform.position.y < skymin) {
             transform.Translate(Vector3.up * skymoveup);
         }
     }
