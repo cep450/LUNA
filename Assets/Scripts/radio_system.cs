957d1083d1ca890c5978bb37dbd4c6dec5930129
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2021-11-14 01:07:55 -0500

    radio_fake_choice

--- Assets/Scripts/radio_system.cs
+++ Assets/Scripts/radio_system.cs
@@ -9,7 +9,7 @@ using UnityEngine.Audio;
 public class radio_system : MonoBehaviour
 {
     // Start is called before the first frame update
-    public static float frequency = 10.0f;
+    public static float frequency = 20.0f;
     //public TextMeshPro textmeshpro;
     public AudioClip[] radioclips;
     public GameObject indicator;
@@ -17,46 +17,57 @@ public class radio_system : MonoBehaviour
     private AudioSource radio;
     public AudioMixer masterMixer;
     public GameObject dial;
-   // public GameObject dial_2;
+    // public GameObject dial_2;
     public Camera orth_cam;
     private int current_song;
     float Current_mos_x = 0.0f;
     public float change_rate;
     private float current_frequency;
     public static int current_playlist;
+    bool song_changed;
+    int changed_song;
+    public static bool song_stay_same = false;
+   
     void Start()
     {
         radio = GetComponent<AudioSource>();
         Debug.Log(radioclips.Length);
         current_song = 0;
         set_volume(frequency);
-        
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        current_frequency= frequency/((radioclips.Length - 1) * 10 + 5);
-        Vector3 mousePos = orth_cam.ScreenToWorldPoint(Input.mousePosition);
        
+        if (song_changed&& TextBoxManager.text_end)
+        {
+            song_changed = false;
+         
+            StartCoroutine(waitforseconds());
+        }
+        current_frequency = frequency / ((radioclips.Length - 1) * 10 + 5);
+        Vector3 mousePos = orth_cam.ScreenToWorldPoint(Input.mousePosition);
+
         if (mouse_click_detector.radio_change_on)
         {
 
             indicator.SetActive(true);
             if (Input.GetMouseButton(0))
             {
-              
-                if (mousePos.x>Current_mos_x)
+
+                if (mousePos.x > Current_mos_x)
                 {
-                    frequency = frequency + change_rate*Time.deltaTime;
+                    frequency = frequency + change_rate * Time.deltaTime;
                     set_volume(frequency);
                     Current_mos_x = mousePos.x;
-                    dial.transform.rotation = Quaternion.Euler(0, 0, -300*current_frequency);
+                    dial.transform.rotation = Quaternion.Euler(0, 0, -300 * current_frequency);
 
                 }
                 if (mousePos.x < Current_mos_x)
                 {
-                    frequency = frequency - change_rate*Time.deltaTime;
+                    frequency = frequency - change_rate * Time.deltaTime;
                     set_volume(frequency);
                     Current_mos_x = mousePos.x;
                     dial.transform.rotation = Quaternion.Euler(0, 0, -300 * current_frequency);
@@ -67,12 +78,13 @@ public class radio_system : MonoBehaviour
 
             }
         }
-        else {
+        else
+        {
             indicator.SetActive(false);
         }
 
-       // Debug.Log(0.55f + ((1.78f - 0.55f) * frequency / ((radioclips.Length - 1) * 10 + 5)));
-        indicator.transform.position = new Vector3(0.55f+((1.85f-0.55f)*current_frequency), indicator.transform.position.y,indicator.transform.position.z);
+        // Debug.Log(0.55f + ((1.78f - 0.55f) * frequency / ((radioclips.Length - 1) * 10 + 5)));
+        indicator.transform.position = new Vector3(0.55f + ((1.85f - 0.55f) * current_frequency), indicator.transform.position.y, indicator.transform.position.z);
         if (frequency < 0)
         {
             frequency = 0;
@@ -115,15 +127,33 @@ public class radio_system : MonoBehaviour
     {
         Debug.Log(frequency);
         current_playlist = frequency;
+        changed_song = frequency;
+        song_changed = true;
         radio.clip = radioclips[frequency];
         radio.Play(0);
         current_song = frequency;
+        song_stay_same = false;
     }
 
     void set_volume(float frequency)
     {
         SetSound(Mathf.Abs(frequency % 10 - 5));
         SetMainSound(Mathf.Abs(frequency % 10 - 5));
+    }
+
+    IEnumerator waitforseconds()
+    {
+
+        // Move the first cube up or down.
+        Debug.Log("test");
+
+        yield return new WaitForSeconds(3.5f);
+
+        if (changed_song == current_playlist)
+        {
+            song_stay_same = true;
+        }
+      
     }
 }
 
